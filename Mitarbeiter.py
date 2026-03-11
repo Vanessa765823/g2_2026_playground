9ca1d694552ac32f4f8a511b3be3b74c5689bc0a
Matteodarin <matteo.da.rin@gmx.ch>
2026-03-11 11:37:13 +0100

    Mitarbeiter Verknüfpung Standort

--- Mitarbeiter.py
+++ Mitarbeiter.py
@@ -1,8 +1,9 @@
 class Mitarbeiter:
-    def __init__(self, name, alter, position):
+    def __init__(self, name, alter, position, standort):
         self.name = name
         self.alter = alter
         self.position = position
+        self.standort = standort
 
     def __str__(self):
         return f"{self.name}, {self.alter} Jahre alt, Position: {self.position}"
--- Standort.py
+++ Standort.py
@@ -1,8 +1,14 @@
+from Mitarbeiter import Mitarbeiter
+
+
 class Standort:
-    def __init__(self, postleitzahl, ortname, adresse):
+    def __init__(self, postleitzahl, ortname, adresse, mitarbeiter_liste=None):
         self.postleitzahl = postleitzahl
         self.ortname = ortname
         self.adresse = adresse
+        self.mitarbeiter_liste = (
+            mitarbeiter_liste if mitarbeiter_liste is not None else []
+        )
 
     def __repr__(self):
         return f"Standort({self.postleitzahl}, {self.ortname}, {self.adresse})"
