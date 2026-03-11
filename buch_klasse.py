1aebf1a2312124779389825120e5db99798ee225
L0EL0E <76394642+L0EL0E@users.noreply.github.com>
2026-03-11 11:42:19 +0100

    Verknüpfung Autor

---
+++ buch_klasse.py
@@ -0,0 +1,10 @@
+from Autor import Autore
+
+class Buch:
+    def __init__(self, titel, autor , erscheinungsjahr, isbn, seitenanzahl):
+        """Initialisiert ein neues Buch-Objekt."""
+        self.titel = titel
+        self.autor = Autore.voller_name
+        self.erscheinungsjahr = erscheinungsjahr
+        self.isbn = isbn
+        self.seitenanzahl = seitenanzahl
